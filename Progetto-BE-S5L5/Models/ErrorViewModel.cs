fefe9e38c734fa0aa212dcb578e4bf32253e29d1
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2025-03-14 09:09:08 +0100

    Aggiungere i file di progetto.

---
+++ Progetto-BE-S5L5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Progetto_BE_S5L5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
